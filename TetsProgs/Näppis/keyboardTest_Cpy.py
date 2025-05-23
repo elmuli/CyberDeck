e238f8e33237afcdec3111f26b61dad3694f38f1
elmuli <elmeri.elovaara@gmail.com>
2025-05-23 13:11:41 +0300

    Updated code

--- TetsProgs/Näppis/keyboardTest_Cpy.py
+++ TetsProgs/Näppis/keyboardTest_Cpy.py
@@ -12,52 +12,74 @@ layout = KeyboardLayoutUS(keyboard)
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
 
+RowInPins=[board.GP0,board.GP1,board.GP2,board.GP3,board.GP4]
+ColumnOutPins=[board.GP5,board.GP6]
 
-Button_in_1 = board.GP0
-Button_in_2 = board.GP1
-Button_in_3 = board.GP2
-Button_out_1 = board.GP3
-Button_out_2 = board.GP4
+rows=[]
+for b, button in enumerate(RowInPins):
+    rows.append(DigitalInOut(button))
+    rows[b].direction = Direction.INPUT
+    rows[b].pull = Pull.DOWN
 
+cols=[]
+for col_, column in enumerate(ColumnOutPins):
+    cols.append(DigitalInOut(column))
+    cols[col_].direction = Direction.OUTPUT
 
-R_in_1 = DigitalInOut(Button_in_1)
-R_in_1.direction = Direction.INPUT
-R_in_1.pull = Pull.DOWN
-R_in_2 = DigitalInOut(Button_in_2)
-R_in_2.direction = Direction.INPUT
-R_in_2.pull = Pull.DOWN
-R_in_3 = DigitalInOut(Button_in_3)
-R_in_3.direction = Direction.INPUT
-R_in_3.pull = Pull.DOWN
-
-C_out_1 = DigitalInOut(Button_out_1)
-C_out_1.direction = Direction.OUTPUT
-C_out_2 = DigitalInOut(Button_out_2)
-C_out_2.direction = Direction.OUTPUT
-
-rows=[R_in_1,R_in_2,R_in_3]
-cols=[C_out_1,C_out_2]
-
-Layer_1 = [["K1", "K2"],
+Layer_1=[["K1", "K2"],
         ["K3", "K4"],
-        ["_","KF"]]
+        ["K5","K6"],
+        ["K7","K8"],
+        ["KF_1","KF_2"]]
 
-Layer_2 = [["R1", "R2"],
+Layer_2=[["R1", "R2"],
         ["R3", "R4"],
-        ["_","KF"]]
+        ["R5","R6"],
+        ["R7","R8"],
+        ["KF_1","KF_2"]]
 
 key_map_1={
-    "K1": Keycode.ONE,
-    "K2": Keycode.TWO,
-    "K3": Keycode.THREE,
-    "K4": Keycode.FOUR
+    "K1": Keycode.Q,
+    "K2": Keycode.W,
+    "K3": Keycode.E,
+    "K4": Keycode.R,
+    "K5": Keycode.T,
+    "K6": Keycode.Y,
+    "K7": Keycode.SPACE,
+    "K8": Keycode.ENTER
 }
 
 key_map_2={
-    "R1": Keycode.FIVE,
-    "R2": Keycode.SIX,
-    "R3": Keycode.SEVEN,
-    "R4": Keycode.EIGHT
+    "R1": Keycode.U,
+    "R2": Keycode.I,
+    "R3": Keycode.O,
+    "R4": Keycode.P,
+    "R5": Keycode.A,
+    "R6": Keycode.S,
+    "R7": Keycode.D,
+    "R8": Keycode.F
+}
+
+key_map_3={
+    "K1": Keycode.G,
+    "K2": Keycode.H,
+    "K3": Keycode.J,
+    "K4": Keycode.K,
+    "K5": Keycode.L,
+    "K6": Keycode.Z,
+    "K7": Keycode.X,
+    "K8": Keycode.C
+}
+
+key_map_4={
+    "K1": Keycode.V,
+    "K2": Keycode.B,
+    "K3": Keycode.N,
+    "K4": Keycode.M,
+    "K5": Keycode.COMMA,
+    "K6": Keycode.KEYPAD_PERIOD,
+    "K7": Keycode.SLASH,
+    "K8": Keycode.BACKSPACE
 }
 
 Pressed_keys = []
@@ -76,6 +98,7 @@ def scan_keys(layer):
             else:
                 if layer[j][i] in Pressed_keys:
                     Pressed_keys.remove(layer[j][i])
+    led.value = True
     return Pressed_keys
 
 Last_keys = []
@@ -83,32 +106,47 @@ def check_keys():
     global Last_keys
     pressed_keys_l1 = scan_keys(Layer_1)
     pressed_keys_l2 = []
-    Fkey = False
+    Fkey1 = False
+    Fkey2 = False
     if pressed_keys_l1:
         for fk in pressed_keys_l1:
-            if fk == "KF":
-                Fkey = True
-        if(not Fkey): 
+            if fk == "KF_1":
+                Fkey1 = True
+            elif fk == "KF_2":
+                Fkey2 = True
+        if(not Fkey1 and not Fkey2): 
             for key in pressed_keys_l1:
                 if key in key_map_1:
                     keyboard.press(key_map_1[key])
-        if(Fkey):
+        if(Fkey1 and not Fkey2):
             pressed_keys_l2 = scan_keys(Layer_2)
             if pressed_keys_l2:
                 for key in (pressed_keys_l2):
                     if key in key_map_2:
                         keyboard.press(key_map_2[key])
+        if(Fkey2 and not Fkey1): 
+            for key in pressed_keys_l1:
+                if key in key_map_3:
+                    keyboard.press(key_map_3[key])
+        if(Fkey2 and Fkey1):
+            for key in pressed_keys_l1:
+                if key in key_map_3:
+                    keyboard.press(key_map_4[key])
     for k in Last_keys:
         if k not in pressed_keys_l1:
             if k in key_map_1:
                 keyboard.release(key_map_1[k])
+            if k in key_map_3:
+                keyboard.release(key_map_3[k])
+            if k in key_map_4:
+                keyboard.release(key_map_4[k])
         if k not in pressed_keys_l2:
             if k in key_map_2:
                 keyboard.release(key_map_2[k])
+    #print(Fkey1," ",Fkey2)
     Last_keys = pressed_keys_l1+pressed_keys_l2
-while True:
     
+while True:
     led.value = False
     check_keys()
-
     time.sleep(0.01)
